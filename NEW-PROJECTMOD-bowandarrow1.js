59eecbc97e5079efcc23671e4b0d36b4d6e45c67
awesomemc2 <edisondieu22@gmail.com>
2016-04-02 17:21:38 -0700

    Create NEW-PROJECTMOD-bowandarrow1.js
    add this dont disable it again

---
+++ NEW-PROJECTMOD-bowandarrow1.js
@@ -0,0 +1,16 @@
+if join
+ if done seting up
+  setup the bow < arrow
+   if done
+    if lag
+     id not use
+end
+
+if bow pulled
+ if arrow goes
+  use.player.fire.tnt("100000")
+   if pleyed pvp
+    if use
+     player.use.arrow("10000900000000")
+end
+  
